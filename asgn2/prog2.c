1c5b8c90a42eb9ec46e27f55f64a21045caba5fd
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-02-09 14:25:02 -0800

    cmd line check works

---
+++ asgn2/prog2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char** argv)
+{
+	int systemWide=0;
+	//check number of arguments
+	if(argc<3||argc>4)
+	{
+		fprintf(stderr,"Usage: prog2 [-s] <executable> <interval> <logFilename>\n");
+		return 1;
+	}
+	//check systemwide
+	if(argc==4)
+	{
+		if(!strcmp(argv[1],"-s"))
+		{
+			systemWide=1;
+		}
+		else
+		{	
+			fprintf(stderr,"Usage: prog2 [-s] <executable> <interval> <logFilename>\n");
+			return 1;
+
+		}
+	}
+	//testing stage 1
+	printf("systemwide status=%d, argc=%d\n",systemWide,argc);
+	return 0;
+}
