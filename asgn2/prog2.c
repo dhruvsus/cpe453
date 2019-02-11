a365ab0e00830372afcd2b387a0e6eca19f83632
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-02-10 23:31:22 -0800

    fixed whitespace issues

--- asgn2/prog2.c
+++ asgn2/prog2.c
@@ -3,7 +3,7 @@
 time_t t;
 char *curT;
 char stat[25], statm[25];
-FILE *pidstatm,*pidstat;
+FILE *pidstatm, *pidstat;
 
 int main(int argc, char **argv)
 {
@@ -11,7 +11,6 @@ int main(int argc, char **argv)
 	long interval = 0;
 	char *logFile;
 	char *executable;
-	
 	//check number of arguments
 	if (argc < 4 || argc > 5)
 	{
@@ -51,7 +50,6 @@ int main(int argc, char **argv)
 		}
 		logFile = argv[3];
 	}
-
 	//fork and exec
 	return forkAndExec(executable, interval, systemWide, logFile);
 }
@@ -61,7 +59,6 @@ int forkAndExec(char *executable, long interval, int systemWide, char *logFileNa
 	pid_t pid = fork();
 	status = pid;
 	FILE *logFile = fopen(logFileName, "a");
-
 	if (pid == 0)
 	{
 		//child
@@ -74,7 +71,6 @@ int forkAndExec(char *executable, long interval, int systemWide, char *logFileNa
 		while (!waitpid(pid, NULL, WNOHANG))
 		{
 			//FILE *logFile = fopen(logFileName, "a");
-			
 			pidstatm = fopen(statm, "r");
 			pidstat = fopen(stat, "r");
 			if (pidstat == NULL || pidstatm == NULL)
@@ -82,7 +78,6 @@ int forkAndExec(char *executable, long interval, int systemWide, char *logFileNa
 				break;
 			}
 			//parent sleeps and then parses files to report stats
-
 			//time
 			/* get the current time */
 			time(&t);
@@ -91,20 +86,17 @@ int forkAndExec(char *executable, long interval, int systemWide, char *logFileNa
 			if (systemWide)
 			{
 				fprintf(logFile, "[%s ", curT);
-				fprintf(logFile, "System  ");
-				getStatData(logFile);
-				getMeminfoData(logFile);
-				getLoadavgData(logFile);
-				getDiskstatsData(logFile);
+				fprintf(logFile, "System ");
+				getStat(logFile);
+				getMeminfo(logFile);
+				getLoadavg(logFile);
+				getDiskstats(logFile);
 				fprintf(logFile, "\n");
 			}
 			fprintf(logFile, "[%s ", curT);
-			fprintf(logFile, "Process(%d)  ", pid);
-
-			getPidStatData(&logFile, &pidstat);
-			getPidStatmData(&logFile, &pidstatm); //wtf how do pass FILE pointers in C?
-
-			/* Close everything  */
+			fprintf(logFile, "Process(%d) ", pid);
+			getPidStat(&logFile, &pidstat);
+			getPidStatm(&logFile, &pidstatm);
 			fclose(pidstatm);
 			fclose(pidstat);
 			fprintf(logFile, "\n");
@@ -113,23 +105,18 @@ int forkAndExec(char *executable, long interval, int systemWide, char *logFileNa
 	}
 	return status;
 }
-void getStatData(FILE *logfile)
+void getStat(FILE *logfile)
 {
 	char *line = NULL;
 	size_t size = 0;
 	char *tmp;
 	FILE *stat;
-
 	stat = fopen(STAT, "r");
-
-	/* get cpu line */
 	getline(&line, &size, stat);
 	fprintf(logfile, "[PROCESS] ");
-	/* skip "cpu" tok */
 	strtok(line, " ");
 	fprintf(logfile, "cpuusermode ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-	/* skip nice */
 	strtok(NULL, " ");
 	fprintf(logfile, " cpusystemmode ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
@@ -141,8 +128,6 @@ void getStatData(FILE *logfile)
 	fprintf(logfile, "%s", strtok(NULL, " "));
 	fprintf(logfile, " softirqservicetime ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-
-	/* skipping over individual cpus */
 	getline(&line, &size, stat);
 	while (strcmp(strtok(line, " "), "intr") != 0)
 	{
@@ -150,143 +135,88 @@ void getStatData(FILE *logfile)
 	}
 	fprintf(logfile, " intr ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-
-	/* grab ctxt */
 	getline(&line, &size, stat);
-
 	fprintf(logfile, " ctxt ");
-	/* skip over ctxt */
 	strtok(line, " ");
-
-	/* getting rid of jerk \n */
 	tmp = strtok(NULL, " ");
 	tmp[strlen(tmp) - 1] = '\0';
 	fprintf(logfile, "%s", tmp);
-
-	/*skip over btime*/
 	getline(&line, &size, stat);
-
-	/* grab processes */
 	getline(&line, &size, stat);
-	/* skip "processes" tok */
 	strtok(line, " ");
-
 	fprintf(logfile, " forks ");
-
-	/* getting rid of jerk \n */
 	tmp = strtok(NULL, " ");
 	tmp[strlen(tmp) - 1] = '\0';
-
 	fprintf(logfile, "%s", tmp);
-
-	/* grab procs_running */
 	getline(&line, &size, stat);
-	/* skip "procs_running" tok */
 	strtok(line, " ");
-
 	fprintf(logfile, " runnable ");
-
-	/* getting rid of jerk \n */
 	tmp = strtok(NULL, " ");
 	tmp[strlen(tmp) - 1] = '\0';
 	fprintf(logfile, "%s", tmp);
-
-	/* grab procs_blocked */
 	getline(&line, &size, stat);
-	/* skip over "procs_blocked" tok */
 	strtok(line, " ");
-
 	fprintf(logfile, " blocked ");
-
-	/* getting rid of jerk \n */
 	tmp = strtok(NULL, " ");
 	tmp[strlen(tmp) - 1] = '\0';
 	fprintf(logfile, "%s", tmp);
-
 	fclose(stat);
 }
 
-void getMeminfoData(FILE *logfile)
+void getMeminfo(FILE *logfile)
 {
 	char *line = NULL;
 	size_t size = 0;
 	FILE *meminfo;
-
 	meminfo = fopen(MEMINFO, "r");
 	fprintf(logfile, " [MEMORY] ");
-
-	/* get MemTotal line */
 	getline(&line, &size, meminfo);
-	/* skip over "MemTotal" tok */
 	strtok(line, " ");
 	fprintf(logfile, "memtotal ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-
-	/* get MemFree line */
 	getline(&line, &size, meminfo);
-	/* skip over "MemFree" tok */
 	strtok(line, " ");
 	fprintf(logfile, " memfree ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-
-	/* skip buffers line */
 	getline(&line, &size, meminfo);
-
-	/* get cached line */
 	getline(&line, &size, meminfo);
 	strtok(line, " ");
 	fprintf(logfile, " cached ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-
-	/* get swapcached line */
 	getline(&line, &size, meminfo);
 	strtok(line, " ");
 	fprintf(logfile, " swapcached ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-
-	/* get active line */
 	getline(&line, &size, meminfo);
 	strtok(line, " ");
 	fprintf(logfile, " active ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-
-	/* get inactive line */
 	getline(&line, &size, meminfo);
 	strtok(line, " ");
 	fprintf(logfile, " inactive ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-
 	fclose(meminfo);
 }
 
-void getDiskstatsData(FILE *logfile)
+void getDiskstats(FILE *logfile)
 {
 	char *line = NULL;
 	size_t size = 0;
 	FILE *diskstats;
 	diskstats = fopen(DISKSTATS, "r");
-
-	/* skipping over unnecessary junk */
 	while (1)
 	{
-		/* grab line */
 		getline(&line, &size, diskstats);
-		/* skip first token */
 		strtok(line, " ");
-		/* skip second token */
 		strtok(NULL, " ");
-
 		if (!strcmp(strtok(NULL, " "), "sda"))
 		{
 			break;
 		}
 	}
-
-	/* line should be sda at this point */
 	fprintf(logfile, " [DISKSTATS(sda)] ");
 	fprintf(logfile, "totalnoreads ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-	/* skip over reads merged */
 	strtok(NULL, " ");
 	fprintf(logfile, " totalsectorsread ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
@@ -294,26 +224,21 @@ void getDiskstatsData(FILE *logfile)
 	fprintf(logfile, "%s", strtok(NULL, " "));
 	fprintf(logfile, " totalnowrites ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-	/* skip over writes merged */
 	strtok(NULL, " ");
 	fprintf(logfile, " nosectorswritten ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
 	fprintf(logfile, " nomswritten ");
 	fprintf(logfile, "%s", strtok(NULL, " "));
-
 	fclose(diskstats);
 }
 
-void getLoadavgData(FILE *logfile)
+void getLoadavg(FILE *logfile)
 {
 	char *line = NULL;
 	size_t size = 0;
 	FILE *loadavg;
-
 	loadavg = fopen(LOADAVG, "r");
-
 	getline(&line, &size, loadavg);
-
 	fprintf(logfile, " [LOADAVG] ");
 	fprintf(logfile, "1min ");
 	fprintf(logfile, "%s", strtok(line, " "));
@@ -324,12 +249,10 @@ void getLoadavgData(FILE *logfile)
 	fclose(loadavg);
 }
 
-void getPidStatData(FILE **logfile, FILE **pidstat)
+void getPidStat(FILE **logfile, FILE **pidstat)
 {
-	//rss here prints a space afer
 	char line[256], *format;
 	int field = 0;
-
 	fprintf(*logfile, "[STAT] ");
 	fgets(line, sizeof(line), *pidstat);
 	format = strtok(line, " \n");
@@ -388,11 +311,10 @@ void getPidStatData(FILE **logfile, FILE **pidstat)
 	}
 }
 
-void getPidStatmData(FILE **logfile, FILE **pidstatm)
+void getPidStatm(FILE **logfile, FILE **pidstatm)
 {
 	char line[256], *format;
 	int field = 0;
-
 	fprintf(*logfile, "[STATM] ");
 	fgets(line, sizeof(line), *pidstatm);
 	format = strtok(line, " \n");
--- asgn2/prog2.h
+++ asgn2/prog2.h
@@ -12,11 +12,11 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <time.h>
-void getStatData(FILE *logfile);
-void getMeminfoData(FILE *logfile);
-void getDiskstatsData(FILE *logfile);
-void getLoadavgData(FILE *logfile);
-void getPidStatData(FILE **logfile, FILE **pidstat);
-void getPidStatmData(FILE **logfile, FILE **pidstatm);
+void getStat(FILE *logfile);
+void getMeminfo(FILE *logfile);
+void getDiskstats(FILE *logfile);
+void getLoadavg(FILE *logfile);
+void getPidStat(FILE **logfile, FILE **pidstat);
+void getPidStatm(FILE **logfile, FILE **pidstatm);
 int forkAndExec(char *executable, long interval, int systemWide, char *logFile);
 #endif
