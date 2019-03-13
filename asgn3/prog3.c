ed9f5127be7a2a49f354cb3e10f5419c55396c2d
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-03-12 23:10:02 -0700

    putting values in physical memory. todo: replacement

--- asgn3/prog3.c
+++ asgn3/prog3.c
@@ -4,12 +4,14 @@
 #include <sys/mman.h>
 #include <math.h>
 #define min(a, b) (((a) < (b)) ? (a) : (b))
-
+void placeInTable(int address, int *pageTable, int *physicalMemory, int pageSize, int numPages, int nBitsFrame, int numFrames);
+int findEmptyFrame(int *physicalMemory, int numFrames);
 int main(int argc, char *argv[])
 {
-    int *addresses, i = 0, numFrames, pageSize, policy, numAddresses, numPages;
-    addresses = mmap(NULL, 500000 * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    int *addresses, *pageTable, *physicalMemory;
+    int j = 0, i = 0, numFrames, pageSize, policy, numAddresses, numPages, nBitsFrame;
     FILE *fd;
+
     if (argc != 5)
     {
         fprintf(stderr, "incorrect number of args\n");
@@ -20,8 +22,12 @@ int main(int argc, char *argv[])
         fprintf(stderr, "file not found\n");
         return 1;
     }
+    addresses = mmap(NULL, 500000 * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
     while (fscanf(fd, "%x", &addresses[i]) != EOF)
         i++;
+    fclose(fd);
+
     numAddresses = i;
     numFrames = atoi(argv[1]);
     pageSize = atoi(argv[2]);
@@ -42,10 +48,72 @@ int main(int argc, char *argv[])
         fprintf(stderr, "incorrect page replacement policy\n");
         return 1;
     }
-    numPages = min((int)(pow(2, 22) / pageSize), (int)pow(2, 16));
-    for(i=0;i<numAddresses;i++){
-        printf("trying to access page number %d\n", (addresses[i]/pageSize)%(int)pow(2,16));
+    numPages = min((int)(pow(2, 22) / pageSize), (int)pow(2, 16)); // each page table entry contains the frame number, the valid bit, and the recent bit
+    pageTable = mmap(NULL, numPages * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    physicalMemory = mmap(NULL, numFrames * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    for (i = 0; i < numFrames; i++)
+        physicalMemory[i] = -1;
+    i = 1;
+    j = 0;
+    while (i < numFrames)
+    {
+        i = i * 2;
+        j++;
+    }
+    nBitsFrame = j;
+    printf("bits needed to represent frames = %d\n", nBitsFrame);
+    //printf("setRecent %d\n", setRecent);
+    //printf("setvalid %d\n", setValid);
+    for (i = 0; i < numAddresses; i++)
+    {
+        placeInTable(addresses[i], pageTable, physicalMemory, pageSize, numPages, nBitsFrame, numFrames);
     }
     printf("numFrames = %d, pageSize = %d, policy = %d, pages = %d\n", numFrames, pageSize, policy, numPages);
     return 0;
+}
+void placeInTable(int address, int *pageTable, int *physicalMemory, int pageSize, int numPages, int nBitsFrame, int numFrames)
+{
+    // generate bitmasks
+    int setRecent = 1 << nBitsFrame;
+    int setValid = setRecent << 1;
+    int pageTableIndex = (address / pageSize) % numPages;
+    printf("page table index = %d\n", pageTableIndex);
+    // check validity of page
+    if (pageTable[pageTableIndex] >> (nBitsFrame + 1) == 1)
+    {
+        // valid, so check frame and then recent bit
+    }
+    else
+    {
+        int entry = 0, emptyFrame;
+        entry = entry | setValid;
+        emptyFrame = findEmptyFrame(physicalMemory, numFrames);
+        if (emptyFrame >= 0)
+        {
+            // found empty frame
+            entry = entry | emptyFrame;
+            physicalMemory[emptyFrame]=address;
+        }
+        else
+        {
+            // replacement
+        }
+        printf("table entry is %d\n", entry);
+        // invalid, so place here
+        // check if there is a free frame, place
+        // address in the free frame and update
+        // page table
+        // else swap
+    }
+}
+int findEmptyFrame(int *physicalMemory, int numFrames)
+{
+    for (int i = 0; i < numFrames; i++)
+    {
+        if (physicalMemory[i] == -1)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
